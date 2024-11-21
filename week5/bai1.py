1e61cd6cbbb99f01e843e812d53f307a1e0d9b1c
Quan <quanxl2k24@gamil.com>
2024-11-22 01:35:26 +0700

    week5

--- week5/bai1.py
+++ week5/bai1.py
@@ -1,3 +1,62 @@
-n = int(input())
-# m = int(input())
-a = (x = int(input()) for _ in range(n))
+n = int(input("Nhap n: "))
+m = int(input("Nhap m: "))
+
+a = [int(input()) for i in range(n)]
+b = [input() for i in range(m)]
+a = tuple(a)
+b = tuple(b)
+
+tmp = 0  
+for x in a:
+    tmp += x
+print(tmp)
+
+tmp = tmp / n  
+tg = 0
+for x in a: 
+    if tmp < x:
+        tg += 1
+print(f'So phan tu lon hon trung binh cong = {tg}')
+
+a_chan = []
+a_le = []
+
+for x in a:
+    if x%2 == 0:
+        a_chan.append(x)
+    else:
+        a_le.append(x)
+a_chan = tuple(a_chan)
+a_le = tuple(a_le)
+print(f"tuple chan = {a_chan}")
+print(f"tuple le = {a_le}")
+
+def dem_xaukitu (b):
+    k = input("nhap ki tu k: ")
+    print(f"so lan k xuat hien = {b.count(k)}")
+dem_xaukitu(b)
+
+def dem_kitu(b):
+    tmp = 0
+    for y in b:
+        if len(y) >= 5:
+            tmp +=1
+    print(tmp)
+dem_kitu(b)
+
+def kethop_ab(a,b):
+    a = list(a)
+    b = list(b)
+    c = []
+    i = 0
+    j = 0
+    while i < n or j < m:
+        if(i < n):
+            c.append(a[i])
+            i += 1
+
+        if j < m:
+            c.append(b[j])
+            j += 1
+    print(c)
+kethop_ab(a,b)
--- week5/bai5.py
+++ week5/bai5.py
@@ -8,9 +8,37 @@ a = {
     'ports':'5000:5000',
     'depends':'db'
 }
-# print(a)
+print(a)
 
 def sua(a):
     a['verson'] = '3.10'
 sua(a)
+print(a)
+
+print(f'gia tri image: {a["image"]}')
+
+a['environment'] = 'PYTHONUNBUFFERED=1'
+print("Sau khi them:")
+print(a)
+
+if "volumes" in a:
+    print("co key volumes")
+else:
+    print("khong co key do")
+
+a.pop("depends")
+print("Sau khi xoa:")
+print(a)
+
+print(f'So phan tu co trong dict:{len(a)}')
+
+if "always" in a.values():
+    print("Co values alway")
+else:
+    print("Khong co values")
+
+new_key=input("Nhap key")
+new_values=input("nhap values")
+
+a[new_key]=new_values
 print(a)
