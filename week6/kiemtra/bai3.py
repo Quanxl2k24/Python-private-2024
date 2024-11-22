bab4d5c8518c53948f5b6fc835268013a6cfc55e
Quan <quanxl2k24@gamil.com>
2024-11-22 19:37:50 +0700

    kiemtra

--- week6/kiemtra/bai3.py
+++ week6/kiemtra/bai3.py
@@ -1,11 +1,21 @@
 a = "2[abc]3[cd]ef"
+b=''
 tmp = 0
-for i in range(len(a)):
+i = 0
+while i < len(a):
     if a[i].isdigit()==1:
+        tg = int(a[i])
         tg_1=0
+
         for j in range(i,len(a)):
             if a[j] == "]":
                 tg_1=j
                 break
-        b = a[i+1:j]*3
+
+        b += a[i+2:j]*tg
+        i = j + 1
+        
+    else:
+        b+=a[i]
+        i+=1
 print(b)
